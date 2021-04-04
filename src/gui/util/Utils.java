cf2e358bfe76aa112f0c48285cce42634818eef1
Victor <vcolofati@gmail.com>
2021-04-04 13:10:05 -0300

    Crud seller implemented

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -20,7 +20,15 @@ public class Utils {
 
 	public static Integer tryParseToInt(String str) {
 		try {
-			return Integer.parseInt(str);
+				return Integer.parseInt(str);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+	}
+	
+	public static Double tryParseToDouble(String str) {
+		try {
+				return Double.parseDouble(str);
 		} catch (NumberFormatException e) {
 			return null;
 		}
